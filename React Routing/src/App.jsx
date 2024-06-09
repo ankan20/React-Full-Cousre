a1727c1a582b5a16f8f0b299d7f28b548a94488f
Ankan Das <127923458+ankan20@users.noreply.github.com>
2024-06-09 18:14:46 +0530

    useNavigate to change route CSR

--- React Routing/src/App.jsx
+++ React Routing/src/App.jsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import React from 'react'
+import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
+import Landing from './Components/Landing'
+import Dashboard from './Components/Dashboard'
 
+const App = () => {
+  return (
+    <>
+    
+    <BrowserRouter>
+    <AppBar/>
+      <Routes>
+        <Route path="/" element={<Landing/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
+      </Routes>
+    </BrowserRouter>
+    </>
+    
+  )
+}
 
-function App() {
-  
+function AppBar (){
+  const navigate = useNavigate(); //use in BrowserRouter
 
   return (
     <>
-      
-     
+    
+    <div >
+      <button onClick={()=>{
+        navigate("/")
+      }}>Landing Page</button>
+      <button onClick={()=>{
+        navigate("/dashboard")
+      }}>Dashboard</button>
+    </div>
+    
     </>
   )
 }
